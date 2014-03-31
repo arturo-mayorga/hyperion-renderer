cb985f5c7ef090ead25e24f88df8c1523e90702e
amayorga <arturo.mayorga@gmail.com>
2014-03-31 18:51:48 -0400

    Separating the GRenderStrategy away from the context.

--- wGl/src/gcontext.js
+++ wGl/src/gcontext.js
@@ -1,31 +1,19 @@
-/**
+/** 
  * @constructor
  */
-function GContext(canvas, shaderSrcMap)
+function GRenderStrategy( gl, shaderSrcMap )
 {
-	this.scene            = undefined;
-	this.gl               = undefined;
-	this.rttFramebuffer   = undefined;
-	this.rttTexture       = undefined;
-	this.screenVertBuffer = undefined;
-	this.screenTextBuffer = undefined;
-	this.screenIndxBuffer = undefined;
-	this.currentProgram   = undefined;
-	
-	var whiteTexture = new GTexture(["white.jpg"], "assets/2d/");
-    
-    this.gl = canvas.getContext("experimental-webgl", { antialias: true } );
-	
-    var gl = this.gl;
-    
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.enable(gl.BLEND);
-    
-    gl.viewportWidth = canvas.width;
-    gl.viewportHeight = canvas.height;
+    this.gl = gl;
     
     this.initTextureFramebuffer();
     this.initShaders(shaderSrcMap);
+    
+    this.initScreenVBOs();
+}
+
+GRenderStrategy.prototype.initScreenVBOs = function()
+{
+    var gl = this.gl;
     
     gl.clearColor(0.1, 0.3, 0.1, 1.0);
     gl.enable(gl.DEPTH_TEST);
@@ -64,48 +52,56 @@ function GContext(canvas, shaderSrcMap)
     this.screenIndxBuffer.numItems = 6;
 	
 	this.hMatrix = mat3.create();
-    
-    whiteTexture.bindToContext(gl);
-    gl.whiteTexture = whiteTexture;
-	
-}
-	
-GContext.prototype.setScene = function (scene_)
-{
-    this.scene = scene_;
-    this.scene.bindToContext(this.gl);
-}
+};
 
-GContext.prototype.setHud = function ( hud_ )
-{
-    this.hud = hud_;
-    this.hud.bindToContext(this.gl);
-}
-
-GContext.prototype.draw = function()
+GRenderStrategy.prototype.initTextureFramebuffer = function()
 {
     var gl = this.gl;
-    this.phongShader.activate();
+    this.rttFramebuffer = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.rttFramebuffer);
-    gl.viewport(0, 0, 1024, 1024);
-    gl.enable(gl.DEPTH_TEST);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);	
-    scene.draw(this.phongShader);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    this.phongShader.deactivate();
+    this.rttFramebuffer.width = 1024;
+    this.rttFramebuffer.height = 1024;
     
-    this.fullScreenProgram.activate();
+    this.rttTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.rttTexture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     
-    this.drawScreenBuffer(this.fullScreenProgram);	
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.rttFramebuffer.width, this.rttFramebuffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
     
-    if (this.hud != undefined)
+    var renderbuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, this.rttFramebuffer.width, this.rttFramebuffer.height);
+    
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.rttTexture, 0);
+    
+    if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE)
     {
-        this.hud.draw(this.fullScreenProgram);
+        alert("incomplete famebuffer");
     }
-    this.fullScreenProgram.deactivate();
-}
+    
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderbuffer);
+    
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+};
 
-GContext.prototype.drawScreenBuffer = function(shader)
+GRenderStrategy.prototype.initShaders = function (shaderSrcMap) 
+{
+    var gl = this.gl;
+      
+    var phong = new GShader(shaderSrcMap["phong-vs.c"], shaderSrcMap["phong-fs.c"]);
+    phong.bindToContext(gl);
+    
+    var fullScr = new GShader(shaderSrcMap["fullscr-vs.c"], shaderSrcMap["fullscr-fs.c"]);
+    fullScr.bindToContext(gl);
+    
+    this.fullScreenProgram = fullScr;
+    this.phongShader = phong;
+};
+
+GRenderStrategy.prototype.drawScreenBuffer = function(shader)
 {
     var gl = this.gl;
 	
@@ -138,53 +134,80 @@ GContext.prototype.drawScreenBuffer = function(shader)
     }
 	
     gl.drawElements(gl.TRIANGLES, this.screenIndxBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+};
+
+GRenderStrategy.prototype.draw = function ( scene, hud )
+{
+    var gl = this.gl;
+    this.phongShader.activate();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.rttFramebuffer);
+    gl.viewport(0, 0, 1024, 1024);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);	
+    scene.draw(this.phongShader);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    this.phongShader.deactivate();
+    
+    this.fullScreenProgram.activate();
+    
+    this.drawScreenBuffer(this.fullScreenProgram);	
+    
+    if (hud != undefined)
+    {
+        hud.draw(this.fullScreenProgram);
+    }
+    this.fullScreenProgram.deactivate();
+};
+
+
+/**
+ * @constructor
+ */
+function GContext(canvas, shaderSrcMap)
+{
+	this.scene            = undefined;
+	this.gl               = undefined;
+	this.rttFramebuffer   = undefined;
+	this.rttTexture       = undefined;
+	this.screenVertBuffer = undefined;
+	this.screenTextBuffer = undefined;
+	this.screenIndxBuffer = undefined;
+	this.currentProgram   = undefined;
+	
+	var whiteTexture = new GTexture(["white.jpg"], "assets/2d/");
+    
+    this.gl = canvas.getContext("experimental-webgl", { antialias: true } );
+	
+    var gl = this.gl;
+    
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.enable(gl.BLEND);
+    
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+    
+    this.renderStrategy = new GRenderStrategy( gl, shaderSrcMap );
+    
+    
+    whiteTexture.bindToContext(gl);
+    gl.whiteTexture = whiteTexture;
+}
+	
+GContext.prototype.setScene = function (scene_)
+{
+    this.scene = scene_;
+    this.scene.bindToContext(this.gl);
 }
 
-GContext.prototype.initTextureFramebuffer = function()
+GContext.prototype.setHud = function ( hud_ )
 {
-    var gl = this.gl;
-    this.rttFramebuffer = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.rttFramebuffer);
-    this.rttFramebuffer.width = 1024;
-    this.rttFramebuffer.height = 1024;
-    
-    this.rttTexture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, this.rttTexture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.rttFramebuffer.width, this.rttFramebuffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-    
-    var renderbuffer = gl.createRenderbuffer();
-    gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
-    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, this.rttFramebuffer.width, this.rttFramebuffer.height);
-    
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.rttTexture, 0);
-    
-    if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE)
-    {
-        alert("incomplete famebuffer");
-    }
-    
-    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderbuffer);
-    
-    gl.bindTexture(gl.TEXTURE_2D, null);
-    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-} 
+    this.hud = hud_;
+    this.hud.bindToContext(this.gl);
+}
 
-GContext.prototype.initShaders = function (shaderSrcMap) 
+GContext.prototype.draw = function()
 {
-    var gl = this.gl;
-      
-    var phong = new GShader(shaderSrcMap["phong-vs.c"], shaderSrcMap["phong-fs.c"]);
-    phong.bindToContext(gl);
-    
-    var fullScr = new GShader(shaderSrcMap["fullscr-vs.c"], shaderSrcMap["fullscr-fs.c"]);
-    fullScr.bindToContext(gl);
-    
-    this.fullScreenProgram = fullScr;
-    this.phongShader = phong;
+    this.renderStrategy.draw(this.scene, this.hud);
 }
 
 

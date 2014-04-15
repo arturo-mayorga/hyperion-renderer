aff68c676dbea354600af22709c82ffc82620f12
amayorga <arturo.mayorga@gmail.com>
2014-04-15 19:38:57 -0400

    more ssao experiments

--- wGl/assets/shaders/light-fs.c
+++ wGl/assets/shaders/light-fs.c
@@ -6,10 +6,25 @@ uniform sampler2D uMapNormal;
 uniform sampler2D uMapPosition;
 uniform sampler2D uMapRGBDepth;
 
+const float numSamples = 16.0;
+
+float randoms(float co){
+  //  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453)*2.0 - 1.0 ;
+  //  return sin(co.x * co.y)*2.0 - 1.0 ;
+    return sin(co * (3.1416/1.5));
+}
+
+float randomc(float co){
+  //  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453)*2.0 - 1.0 ;
+  //  return sin(co.x * co.y)*2.0 - 1.0 ;
+    return cos(co * (3.1416/1.5));
+}
 
 float random(vec2 co){
     return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453)*2.0 - 1.0 ;
 }
+
+
 
 void main(void)
 {
@@ -17,36 +32,47 @@ void main(void)
     vec3 tv3Position = texture2D(uMapPosition, vTexCoordinate).xyz;
     vec3 tv3RGBDepth = texture2D(uMapRGBDepth, vTexCoordinate).xyz;
 	vec3 tv3Color    = texture2D(uMapKd,       vTexCoordinate).xyz;
-	
-	vec3 rvec =  normalize(vec3(random(vTexCoordinate),random(vTexCoordinate*23.0),random(vTexCoordinate*36.0)))* 2.0 - 1.0;
 
 	float tfdDepthValue = tv3Position.z; //256.0 * (tv3RGBDepth.x + tv3RGBDepth.y);
 	
 
-	float uRadius = 0.10/tfdDepthValue; 
+	// there is greater sampling penalty for larger radii
+    float uRadius = min (0.06, 0.10/tfdDepthValue); 
 	
-	const float numSamples = 10.0;
+	
 	
 	float occlusion = 0.0;
+	vec3 occlusionC = vec3(0);
     for (float i = 0.0; i < numSamples; ++i) 
     {
         // get sample position:
+        
+        // the commented out approach produces faster results but the other has
+        // beter sampling distibution
+        //vec2 sample = vec2(randoms(i), randomc(i)) * (i+1.0)/numSamples;   
         vec2 sample = normalize(vec2(random(vec2(vTexCoordinate*i)), random(vTexCoordinate*i*32.43))) * random(vTexCoordinate*i*45.6);
+        
         
         sample = sample * uRadius + vTexCoordinate;
         
         // get sample depth:
         float sampleDepth = texture2D(uMapPosition, sample).z;
+        vec4 sampleColor = texture2D(uMapKd, sample);
+        vec4 sampleNormal = texture2D(uMapNormal, sample);
+        
+        float normalAtt = 1.0;//dot(sampleNormal.xyz, tv3Normal) < 0.059?1.0:0.0;
+        //normalAtt *= normalAtt;
         
         // range check & accumulate:
-        float rangeCheck =  abs(tfdDepthValue - sampleDepth) < 0.2 ? 1.0 : 0.2;
-        occlusion += (tfdDepthValue > sampleDepth ? 1.0 : 0.0) * rangeCheck;
+        vec3 incCol = 1.0 - sampleColor.xyz*0.5;
+        float rangeCheck =  abs(tfdDepthValue - sampleDepth) < 0.2 ? normalAtt : 0.0;
+        occlusionC += incCol.xyz * vec3((tfdDepthValue >= sampleDepth ? 1.0 : 0.0) * rangeCheck);
     }
     
-    float oFactor = 1.0 - (occlusion/numSamples)*0.5;
-   // oFactor *= oFactor*oFactor;
+    vec3 ovFactor = 1.0 - (occlusionC/numSamples);
     
-    gl_FragColor = vec4(tv3Color*oFactor, 1);
+    gl_FragColor = vec4(tv3Color*ovFactor, 1);
     // gl_FragColor = vec4(vec3(oFactor), 1);
+    //gl_FragColor = vec4(ovFactor, 1);
 } 
 

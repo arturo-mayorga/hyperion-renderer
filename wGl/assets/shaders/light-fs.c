5765caa185515a6fb505ab9023eaa4691a06249f
amayorga <arturo.mayorga@gmail.com>
2014-04-16 02:10:22 -0400

    Some SSAO optimisations

--- wGl/assets/shaders/light-fs.c
+++ wGl/assets/shaders/light-fs.c
@@ -6,24 +6,77 @@ uniform sampler2D uMapNormal;
 uniform sampler2D uMapPosition;
 uniform sampler2D uMapRGBDepth;
 
-const float numSamples = 16.0;
+const float numSamples = 4.0;
 
-float randoms(float co){
+float pw = 1.0/1280.0*0.5; 
+float ph = 1.0/720.0*0.5;
+
+//float pw = 1.0/800.0*0.5; 
+//float ph = 1.0/600.0*0.5;
+
+float randoms(float co)
+{
   //  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453)*2.0 - 1.0 ;
   //  return sin(co.x * co.y)*2.0 - 1.0 ;
     return sin(co * (3.1416/1.5));
 }
 
-float randomc(float co){
+float randomc(float co)
+{
   //  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453)*2.0 - 1.0 ;
   //  return sin(co.x * co.y)*2.0 - 1.0 ;
     return cos(co * (3.1416/1.5));
 }
 
-float random(vec2 co){
+float random(vec2 co)
+{
     return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453)*2.0 - 1.0 ;
 }
 
+
+float readDepth(in vec2 coord)  
+{  
+    if (coord.x<0.0||coord.y<0.0) return 1.0;
+    
+    float posZ = texture2D(uMapPosition, coord).z;   
+    return posZ;  
+} 
+
+
+vec3 calAO(float depth, vec2 sample)
+{
+    float temp = 0.0;
+    float temp2 = 0.0;
+    float coordw = vTexCoordinate.x + sample.x/depth;
+    float coordh = vTexCoordinate.y + sample.y/depth;
+    float coordw2 = vTexCoordinate.x - sample.x/depth;
+    float coordh2 = vTexCoordinate.y - sample.y/depth;
+    
+    if (coordw  < 1.0 && coordw  > 0.0 && coordh < 1.0 && coordh  > 0.0)
+    {
+        vec2 coord = vec2(coordw , coordh);
+        vec2 coord2 = vec2(coordw2, coordh2);
+        
+        // get sample depth:
+        float sampleDepth = readDepth(coord);
+        
+        
+        float normalAtt = 1.0;
+        
+        // range check & accumulate:
+        
+        float rangeCheck =  abs(depth - sampleDepth) < 0.2 ? normalAtt : 0.0;
+        
+        if (depth >= sampleDepth)
+        {
+            vec4 sampleColor = texture2D(uMapKd, coord);
+            vec3 incCol = 1.0 - sampleColor.xyz*0.5;
+            return incCol.xyz * rangeCheck;
+        }
+    }
+    
+    return vec3(0);
+}
 
 
 void main(void)
@@ -38,6 +91,8 @@ void main(void)
 
 	// there is greater sampling penalty for larger radii
     float uRadius = min (0.06, 0.10/tfdDepthValue); 
+    
+    vec3 random = vec3(random(vTexCoordinate), random(vTexCoordinate*32.43), random(vTexCoordinate*45.6));
 	
 	
 	
@@ -45,34 +100,31 @@ void main(void)
 	vec3 occlusionC = vec3(0);
     for (float i = 0.0; i < numSamples; ++i) 
     {
-        // get sample position:
+        occlusionC += calAO(tfdDepthValue, vec2(pw, ph));
+        occlusionC += calAO(tfdDepthValue, vec2(pw, -ph));
+        occlusionC += calAO(tfdDepthValue, vec2(-pw, ph));
+        occlusionC += calAO(tfdDepthValue, vec2(-pw, -ph));
+            
+        occlusionC += calAO(tfdDepthValue, vec2(pw*1.2, ph));
+        occlusionC += calAO(tfdDepthValue, vec2(-pw*1.2, ph));
+        occlusionC += calAO(tfdDepthValue, vec2(0, ph*1.2));
+        occlusionC += calAO(tfdDepthValue, vec2(0, -ph*1.2));
+            
+        //sample jittering:
+        pw += random.x*0.0007;
+        ph += random.y*0.0007;
         
-        // the commented out approach produces faster results but the other has
-        // beter sampling distibution
-        //vec2 sample = vec2(randoms(i), randomc(i)) * (i+1.0)/numSamples;   
-        vec2 sample = normalize(vec2(random(vec2(vTexCoordinate*i)), random(vTexCoordinate*i*32.43))) * random(vTexCoordinate*i*45.6);
-        
-        
-        sample = sample * uRadius + vTexCoordinate;
-        
-        // get sample depth:
-        float sampleDepth = texture2D(uMapPosition, sample).z;
-        vec4 sampleColor = texture2D(uMapKd, sample);
-        vec4 sampleNormal = texture2D(uMapNormal, sample);
-        
-        float normalAtt = 1.0;//dot(sampleNormal.xyz, tv3Normal) < 0.059?1.0:0.0;
-        //normalAtt *= normalAtt;
-        
-        // range check & accumulate:
-        vec3 incCol = 1.0 - sampleColor.xyz*0.5;
-        float rangeCheck =  abs(tfdDepthValue - sampleDepth) < 0.2 ? normalAtt : 0.0;
-        occlusionC += incCol.xyz * vec3((tfdDepthValue >= sampleDepth ? 1.0 : 0.0) * rangeCheck);
+        //increase sampling area:
+        pw *= 1.7;  
+        ph *= 1.7;    
     }
     
-    vec3 ovFactor = 1.0 - (occlusionC/numSamples);
+    vec3 ovFactor = 1.0 - (occlusionC/(numSamples*8.0));
     
     gl_FragColor = vec4(tv3Color*ovFactor, 1);
     // gl_FragColor = vec4(vec3(oFactor), 1);
-    //gl_FragColor = vec4(ovFactor, 1);
+    // gl_FragColor = vec4(ovFactor, 1);
 } 
+
+
 

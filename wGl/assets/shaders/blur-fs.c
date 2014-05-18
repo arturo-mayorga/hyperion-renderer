2eed5e1941013caf1cc9ce4113c02e9f63a19332
amayorga <arturo.mayorga@gmail.com>
2014-05-18 12:52:45 -0400

    Reducing the number of varying vectors.

--- wGl/assets/shaders/blur-fs.c
+++ wGl/assets/shaders/blur-fs.c
@@ -3,26 +3,20 @@ precision mediump float;
 
 uniform sampler2D uMapKd;
 varying vec2 vTexCoordinate;
-varying vec2 vBlurTexCoords[14];
+varying vec2 vBlurTexCoords[8];
 
 void main(void)
 {
     gl_FragColor = vec4(0.0);
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 0])*0.0044299121055113265;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 1])*0.00895781211794;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 2])*0.0215963866053;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 3])*0.0443683338718;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 4])*0.0776744219933;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 5])*0.115876621105;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 6])*0.147308056121;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[0])*0.0044299121055113265;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[1])*0.0215963866053;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[2])*0.115876621105;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[3])*0.147308056121;
     gl_FragColor += texture2D(uMapKd, vTexCoordinate    )*0.159576912161;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 7])*0.147308056121;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 8])*0.115876621105;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[ 9])*0.0776744219933;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[10])*0.0443683338718;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[11])*0.0215963866053;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[12])*0.00895781211794;
-    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[13])*0.0044299121055113265;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[4])*0.147308056121;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[5])*0.115876621105;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[6])*0.0215963866053;
+    gl_FragColor += texture2D(uMapKd, vBlurTexCoords[7])*0.0044299121055113265;
 } 
 
 
--- wGl/assets/shaders/blur-vs.c
+++ wGl/assets/shaders/blur-vs.c
@@ -4,26 +4,20 @@ attribute vec2 aTextureVertex;
 uniform mat3 uHMatrix;
 
 varying vec2 vTexCoordinate;
-varying vec2 vBlurTexCoords[14];
+varying vec2 vBlurTexCoords[8];
 
 void main(void)
 {
 	gl_Position = vec4( (uHMatrix * aPositionVertex.xyz), 1);
 	
 	vTexCoordinate = aTextureVertex;
-	vBlurTexCoords[ 0] = vTexCoordinate + vec2(-0.028, 0.0);
-    vBlurTexCoords[ 1] = vTexCoordinate + vec2(-0.024, 0.0);
-    vBlurTexCoords[ 2] = vTexCoordinate + vec2(-0.020, 0.0);
-    vBlurTexCoords[ 3] = vTexCoordinate + vec2(-0.016, 0.0);
-    vBlurTexCoords[ 4] = vTexCoordinate + vec2(-0.012, 0.0);
-    vBlurTexCoords[ 5] = vTexCoordinate + vec2(-0.008, 0.0);
-    vBlurTexCoords[ 6] = vTexCoordinate + vec2(-0.004, 0.0);
-    vBlurTexCoords[ 7] = vTexCoordinate + vec2( 0.004, 0.0);
-    vBlurTexCoords[ 8] = vTexCoordinate + vec2( 0.008, 0.0);
-    vBlurTexCoords[ 9] = vTexCoordinate + vec2( 0.012, 0.0);
-    vBlurTexCoords[10] = vTexCoordinate + vec2( 0.016, 0.0);
-    vBlurTexCoords[11] = vTexCoordinate + vec2( 0.020, 0.0);
-    vBlurTexCoords[12] = vTexCoordinate + vec2( 0.024, 0.0);
-    vBlurTexCoords[13] = vTexCoordinate + vec2( 0.028, 0.0);
+	vBlurTexCoords[0] = vTexCoordinate + vec2(-0.028, 0.0);
+    vBlurTexCoords[1] = vTexCoordinate + vec2(-0.020, 0.0);
+    vBlurTexCoords[2] = vTexCoordinate + vec2(-0.008, 0.0);
+    vBlurTexCoords[3] = vTexCoordinate + vec2(-0.004, 0.0);
+    vBlurTexCoords[4] = vTexCoordinate + vec2( 0.004, 0.0);
+    vBlurTexCoords[5] = vTexCoordinate + vec2( 0.008, 0.0);
+    vBlurTexCoords[6] = vTexCoordinate + vec2( 0.020, 0.0);
+    vBlurTexCoords[7] = vTexCoordinate + vec2( 0.028, 0.0);
 } 
 

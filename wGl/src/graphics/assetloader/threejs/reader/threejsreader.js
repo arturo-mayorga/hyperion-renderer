c5fbf32958ed6e269e3c8b878aaa1d1214ea201e
amayorga <arturo.mayorga@gmail.com>
2014-05-27 00:36:34 -0400

    adding ThreejsLoader/Reader stub files.

---
+++ wGl/src/graphics/assetloader/threejs/reader/threejsreader.js
@@ -0,0 +1,81 @@
+// Copyright (C) 2014 Arturo Mayorga
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+
+/**
+ * @interface
+ */
+function ThreejsReaderObserver () {}
+
+/**
+ * This function is called whenever a new GeometryTriMesh object is loaded
+ * @param {GeometryTriMesh} New object that was just made available
+ */
+ThreejsReaderObserver.prototype.onNewMeshAvailable = function ( mesh ) {};
+
+/**
+ * @constructor
+ * @param {string} Path to the location of this obj file's resources
+ * @param {Array.<string>} Lines of the obj file
+ * @param {GScene} Target scene for the loading process
+ * @param {GGroup} Target group for the loading process
+ * @param {ThreejsReaderObserver} Observer to the loading process
+ */
+function ThreejsReader( path, objStrA, scene, group, observer )
+{
+	/**
+	 * @struct
+	 */
+	this.IndexRecord = function( string )
+	{
+		var tokens = string.split("/");
+		this.vertIdx = parseFloat(tokens[0])-1;
+		this.textIdx = parseFloat(tokens[1])-1;
+		this.normIdx = parseFloat(tokens[2])-1;			
+	}
+
+	this.objGVerts = [];
+	this.objTVerts = [];
+	this.objNormals = [];
+	this.currentMesh = undefined;
+	this.scene = scene;
+	this.group = group;
+	this.path = path;
+	this.objStrA = objStrA;
+	this.observer = observer;
+
+	this.currentIndex = 0;
+	
+	this.groupMap = {};
+	
+	this.invertNormals = false;
+	this.isLoadComplete = false;
+	this.updateIndex = 0;
+	this.polyCount = 0;
+	
+}
+
+/**
+ * Advance through the loading process
+ * @param {number} Milliseconds since the last update
+ */
+ThreejsReader.prototype.update = function (time)
+{
+};
+ 
---
+++ wGl/src/graphics/assetloader/threejs/threejsloader.js
@@ -0,0 +1,155 @@
+// Copyright (C) 2014 Arturo Mayorga
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+
+/**
+ * @interface
+ */
+function ThreejsLoaderObserver () {}
+
+/**
+ * This function gets called whenever the observed loader completes the loading process
+ * @param {ThreejsLoader} Loader object completing the load operation.
+ */
+ThreejsLoaderObserver.prototype.onObjLoaderCompleted = function ( loader ) {};
+
+/**
+ * @param {ThrejsLoader}
+ * @param {number} progress Progress value
+ */
+ThreejsLoaderObserver.prototype.onObjLoaderProgress = function ( loader, progress ) {};
+
+/**
+ * @constructor
+ * @implements {ThreejsReaderObserver}
+ * @param {GScene} Target scene for this loader
+ * @param {GGroup} Target group for this loader
+ */
+function ThrejsLoader( scene, group )
+{
+	this.client = new XMLHttpRequest();
+	this.scene = scene;
+	this.group = group;
+	this.isDownloadComplete = false;
+	this.isReaderReady = false;
+	this.isReadComplete = false;
+	this.availableTime = 17;
+	this.downloadProgress = 0;
+	this.totalProgress = 0;
+}
+
+
+
+/**
+ * This function loads a json file
+ * @param {string} Path for the json file and it's resources
+ * @source {string} json file that needs to be loaded
+ */
+ThreejsLoader.prototype.loadJson = function ( path, source )
+{
+    this.isDownloadComplete = false;
+    this.client.open('GET', path + source);
+	this.client.responseType = "json";
+    this.currentPath = path;
+    this.client.onload = function(e) 
+    {
+		var status = this.client.status;
+		if (status == 200) {
+			this.jsonToRead = this.client.response;
+		} else {
+			// ...something went wrong.
+		}
+    }.bind(this);
+    this.client.send();
+};
+
+/**
+ * Update this json loader
+ * @param {number} Milliseconds since the last update
+ */
+ThreejsLoader.prototype.update = function ( time )
+{
+    var timeStart = new Date().getTime();
+    
+    var targetTime = 17;
+    
+    if (time > targetTime)
+    {
+        --this.availableTime;
+    }
+    else
+    {
+        ++this.availableTime;
+    }
+	
+	if ( this.availableTime <= 0 )
+	{
+		this.availableTime = 5;
+	}
+	
+	this.totalProgress = 0;
+	
+	if (this.observer != undefined)
+	{
+		this.observer.onObjLoaderProgress(this, this.totalProgress);
+	}
+    
+    while ( ( (new Date().getTime()) - timeStart) < this.availableTime )
+    {
+		
+    }
+};
+
+/**
+ * This function is called whenever a new GeometryTriMesh object is loaded
+ * @param {GeometryTriMesh} New object that was just made available
+ */
+ThreejsLoader.prototype.onNewMeshAvailable = function ( mesh )
+{
+	this.sendMeshToGroup( mesh ); 
+};
+
+/**
+ * This function is called whenever a new GeometryTriMesh object is 
+ * loaded and needs to be sent directly to the scene.
+ * @param {GeometryTriMesh} New object that was just made available
+ */
+ThreejsLoader.prototype.sendMeshToGroup = function ( mesh )
+{
+   var obj = new GObject(mesh.getVertBuffer(),
+						  mesh.getTVerBuffer(),
+						  mesh.getNormBuffer(),
+						  mesh.indices,
+						  mesh.getName());
+                                      
+	obj.setMtlName(mesh.getMtlName());
+	this.group.addChild(obj); 
+};
+
+/**
+ * Set the observer for the loader
+ * @param {ThreejsLoaderObserver} observer Observer that receives updates
+ */
+ThreejsLoader.prototype.setObserver = function ( observer )
+{
+    this.observer = observer;
+};
+	
+
+

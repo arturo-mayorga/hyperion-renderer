987bddbcc63512f62079b78407e80efa00267c2f
arturo-mayorga <arturo.mayorga@gmail.com>
2014-07-02 21:21:29 -0400

    FXAA GLSL implementation

--- wGl/assets/shaders/fullscr-fs.c
+++ wGl/assets/shaders/fullscr-fs.c
@@ -24,12 +24,95 @@ varying vec2 vTexCoordinate;
 uniform sampler2D uMapKd;
 uniform vec4 uKd;
 
+
+float rt_w = 1280.0;
+float rt_h = 720.0; 
+
+float FXAA_SPAN_MAX = 8.0;
+float FXAA_REDUCE_MUL = 1.0/8.0;
+float FXAA_SUBPIX_SHIFT = 1.0/4.0;
+
+#define FxaaInt2 vec2
+#define FxaaFloat2 vec2
+#define FxaaTexLod0(t, p) texture2D(t, p)
+#define FxaaTexOff(t, p, o, r) texture2D(t, p + o * r)
+
+
+// based on [http://www.geeks3d.com/20110405/fxaa-fast-approximate-anti-aliasing-demo-glsl-opengl-test-radeon-geforce/3/]
+vec3 FxaaPixelShader( 
+  vec4 posPos, // Output of FxaaVertexShader interpolated across screen.
+  sampler2D tex, // Input texture.
+  vec2 rcpFrame) // Constant {1.0/frameWidth, 1.0/frameHeight}.
+{   
+/*---------------------------------------------------------*/
+    #define FXAA_REDUCE_MIN   (1.0/128.0)
+    //#define FXAA_REDUCE_MUL   (1.0/8.0)
+    //#define FXAA_SPAN_MAX     8.0
+/*---------------------------------------------------------*/
+    vec3 rgbNW = FxaaTexLod0(tex, posPos.zw).xyz;
+    vec3 rgbNE = FxaaTexOff(tex, posPos.zw, FxaaInt2(1.0,0.0), rcpFrame.xy).xyz;
+    vec3 rgbSW = FxaaTexOff(tex, posPos.zw, FxaaInt2(0.0,1.0), rcpFrame.xy).xyz;
+    vec3 rgbSE = FxaaTexOff(tex, posPos.zw, FxaaInt2(1.0,1.0), rcpFrame.xy).xyz;
+    vec3 rgbM  = FxaaTexLod0(tex, posPos.xy).xyz;
+/*---------------------------------------------------------*/
+    vec3 luma = vec3(0.299, 0.587, 0.114);
+    float lumaNW = dot(rgbNW, luma);
+    float lumaNE = dot(rgbNE, luma);
+    float lumaSW = dot(rgbSW, luma);
+    float lumaSE = dot(rgbSE, luma);
+    float lumaM  = dot(rgbM,  luma);
+/*---------------------------------------------------------*/
+    float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
+    float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
+/*---------------------------------------------------------*/
+    vec2 dir; 
+    dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
+    dir.y =  ((lumaNW + lumaSW) - (lumaNE + lumaSE));
+/*---------------------------------------------------------*/
+    float dirReduce = max(
+        (lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * FXAA_REDUCE_MUL),
+        FXAA_REDUCE_MIN);
+    float rcpDirMin = 1.0/(min(abs(dir.x), abs(dir.y)) + dirReduce);
+    dir = min(FxaaFloat2( FXAA_SPAN_MAX,  FXAA_SPAN_MAX), 
+          max(FxaaFloat2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX), 
+          dir * rcpDirMin)) * rcpFrame.xy;
+/*--------------------------------------------------------*/
+    vec3 rgbA = (1.0/2.0) * (
+        FxaaTexLod0(tex, posPos.xy + dir * (1.0/3.0 - 0.5)).xyz +
+        FxaaTexLod0(tex, posPos.xy + dir * (2.0/3.0 - 0.5)).xyz);
+    vec3 rgbB = rgbA * (1.0/2.0) + (1.0/4.0) * (
+        FxaaTexLod0(tex, posPos.xy + dir * (0.0/3.0 - 0.5)).xyz +
+        FxaaTexLod0(tex, posPos.xy + dir * (3.0/3.0 - 0.5)).xyz);
+    float lumaB = dot(rgbB, luma);
+    if((lumaB < lumaMin) || (lumaB > lumaMax)) return rgbA;
+    return rgbB; }
+
+vec3 PostFX(sampler2D tex, vec2 uv, float time)
+{
+  vec2 rcpFrame = vec2(1.0/rt_w, 1.0/rt_h);
+  
+  vec4 posPos;
+  
+   posPos.xy = uv.xy;
+  posPos.zw = uv.xy - 
+                  (rcpFrame * (0.5 + FXAA_SUBPIX_SHIFT));
+  
+  return FxaaPixelShader(posPos, tex, rcpFrame);
+}
+
+
 void main(void)
 {
-	vec3 tColor = texture2D(uMapKd, vTexCoordinate).xyz;
+	//vec3 tColor = texture2D(uMapKd, vTexCoordinate).xyz;
+	vec3 tColor = PostFX(uMapKd, vTexCoordinate, 0.0);
+	
 	vec3 fColor = vec3( min(tColor.x, uKd.x), 
 	                    min(tColor.y, uKd.y), 
 	                    min(tColor.z, uKd.z) );
+	
+	
+	
+	
 	gl_FragColor = vec4(fColor, uKd.a);
 } 
 

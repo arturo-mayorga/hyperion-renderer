bea4269655e6e9e36a4b18182e16b760c978699a
arturo-mayorga <arturo.mayorga@gmail.com>
2014-07-03 23:09:04 -0400

    Moving the fxaa shader to it's own file.

--- wGl/assets/shaders/fxaa-vs.c
+++ wGl/assets/shaders/fxaa-vs.c
@@ -18,18 +18,16 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
 // SOFTWARE.
 
-precision mediump float;
+attribute vec3 aPositionVertex;
+attribute vec2 aTextureVertex;
+
+uniform mat3 uHMatrix;
 
 varying vec2 vTexCoordinate;
-uniform sampler2D uMapKd;
-uniform vec4 uKd;
 
 void main(void)
 {
-	vec3 tColor = texture2D(uMapKd, vTexCoordinate).xyz;
-	vec3 fColor = vec3( min(tColor.x, uKd.x), 
-	                    min(tColor.y, uKd.y), 
-	                    min(tColor.z, uKd.z) );
-	gl_FragColor = vec4(fColor, uKd.a);
+	vTexCoordinate = aTextureVertex;
+	gl_Position = vec4( (uHMatrix * aPositionVertex.xyz), 1);
 } 
 

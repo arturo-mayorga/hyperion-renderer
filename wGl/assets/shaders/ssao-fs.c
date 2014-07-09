5251e8fdf1b9dca6e6e00c861a60a7191257ee3b
arturo-mayorga <arturo.mayorga@gmail.com>
2014-07-09 07:16:33 -0400

    Updates to use less samples.

--- wGl/assets/shaders/ssao-fs.c
+++ wGl/assets/shaders/ssao-fs.c
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
 // SOFTWARE.
 
+#extension GL_OES_standard_derivatives : enable
 precision mediump float;
 
 varying vec2 vTexCoordinate;
@@ -32,7 +33,14 @@ float pw = 1.0/1024.0*0.5;
 float ph = 1.0/1024.0*0.5;
 
 
+#define NUM_SAMPLES           4.0
+#define NUM_SAMPLES_i           4
+#define NUM_SPIRAL_TURNS      7
+#define VARIATION             1
+#define PI 3.1415926535897932384626433832795
+#define INVERSE_PI 1.0/3.1415926535897932384626433832795
 
+ const float uSampleRadiusWS = 10.0;
 
 float readDepth(in vec2 coord)  
 {  
@@ -107,11 +115,86 @@ vec3 calAO(float depth, vec2 sample)
         {
             vec4 sampleColor = texture2D(uMapKd, coord);
             vec3 incCol = 1.0 - sampleColor.xyz*0.25;
-            return incCol.xyz * temp;
+            return vec3( temp);
         }
     }
     
     return vec3(0);
+}
+
+vec3 getOffsetPositionVS(vec2 uv, vec2 unitOffset, float radiusSS) 
+{
+  uv = uv + radiusSS * unitOffset * (1.0 / vec2(1280.0,720.0));
+   
+  return texture2D(uMapPosition, uv).xyz;
+}
+
+// returns a unit vector and a screen-space radius for the tap on a unit disk
+// (the caller should scale by the actual disk radius)
+vec2 tapLocation(int sampleNumber, float spinAngle, out float radiusSS) 
+{
+  // radius relative to radiusSS
+  float alpha = (float(sampleNumber) + 0.5) * (1.0 / float(NUM_SAMPLES));
+  float angle = alpha * (float(NUM_SPIRAL_TURNS) * 6.28) + spinAngle;
+   
+  radiusSS = alpha;
+  return vec2(cos(angle), sin(angle));
+}
+
+float sampleAO(vec2 uv, vec3 positionVS, vec3 normalVS, float sampleRadiusSS,
+               int tapIndex, float rotationAngle)
+{
+  const float epsilon = 0.9;
+ 
+  const float uBias = 0.0;
+  float radius2 = uSampleRadiusWS * uSampleRadiusWS;
+  
+  
+   
+  // offset on the unit disk, spun for this pixel
+  float radiusSS;
+  vec2 unitOffset = tapLocation(tapIndex, rotationAngle, radiusSS);
+  radiusSS *= sampleRadiusSS;
+   
+  vec3 Q = getOffsetPositionVS(uv, unitOffset, radiusSS);
+  vec3 v = Q - positionVS;
+   
+  float vv = dot(v, v);
+  float vn = dot(v, normalVS) - uBias;
+  
+  //gl_FragColor = vec4(vec3(float(vv < radius2) * max(vn / (epsilon + vv), 0.0)), 1.0);
+   
+#if VARIATION == 0
+   
+  // (from the HPG12 paper)
+  // Note large epsilon to avoid overdarkening within cracks
+  return float(vv < radius2) * max(vn / (epsilon + vv), 0.0);
+   
+#elif VARIATION == 1 // default / recommended
+   
+  // Smoother transition to zero (lowers contrast, smoothing out corners). [Recommended]
+  float f = max(radius2 - vv, 0.0) / radius2;
+  return f * f * f * max(vn / (epsilon + vv), 0.0);
+   
+#elif VARIATION == 2
+   
+  // Medium contrast (which looks better at high radii), no division.  Note that the
+  // contribution still falls off with radius^2, but we've adjusted the rate in a way that is
+  // more computationally efficient and happens to be aesthetically pleasing.
+  float invRadius2 = 1.0 / radius2;
+  return 4.0 * max(1.0 - vv * invRadius2, 0.0) * max(vn, 0.0);
+   
+#else
+   
+  // Low contrast, no division operation
+  return 2.0 * float(vv < radius2) * max(vn, 0.0);
+   
+#endif
+}
+
+vec3 reconstructNormalVS(vec3 positionVS) 
+{
+  return normalize(cross(dFdx(positionVS), dFdy(positionVS)));
 }
 
 
@@ -124,8 +207,25 @@ void main(void)
 
 	float tfdDepthValue = readDepth(vTexCoordinate);  
 	
-	vec3 occlusionC = vec3(0);
-    for (float i = 0.0; i < numSamples; ++i) 
+	tv3Normal = reconstructNormalVS(tv3Position);
+	////
+   
+  float randomPatternRotationAngle = 2.0 * PI * random.x * random.y;
+   
+  float radiusSS  = 0.0; // radius of influence in screen space
+  float radiusWS  = 0.0; // radius of influence in world space
+  float occlusion = 0.0;
+   
+  // TODO (travis): don't hardcode projScale
+  float projScale = 40.0;//1.0 / (2.0 * tan(uFOV * 0.5));
+  radiusWS = uSampleRadiusWS;
+  radiusSS = projScale * radiusWS / tv3Position.z;
+   
+  
+  ////
+	
+	/*vec3 occlusionC = vec3(0);
+    for (float i = 0.0; i < NUM_SAMPLES; ++i) 
     {
         occlusionC += calAO(tfdDepthValue, vec2(pw, ph));
         occlusionC += calAO(tfdDepthValue, vec2(pw, -ph));
@@ -147,14 +247,22 @@ void main(void)
         
         pw *= 3.5;  
         ph *= 3.5;
-    }
+    }*/
     
-    vec3 ovFactor = 1.0-(occlusionC/(numSamples*8.0));
+    vec3 ovFactor;// = (occlusionC/(NUM_SAMPLES*8.0));
     
     //gl_FragColor = vec4(tv3Color*ovFactor, 1);
-    // gl_FragColor = vec4(vec3(oFactor), 1);
+     //gl_FragColor = vec4(vec3(oFactor), 1);
     gl_FragColor = vec4(ovFactor, 1);
     //gl_FragColor = vec4(random_, 1);
+    
+    for (int i = 0; i < NUM_SAMPLES_i; ++i) {
+    occlusion += sampleAO(vTexCoordinate, tv3Position, tv3Normal, radiusSS, i,
+                          randomPatternRotationAngle);
+    }
+    
+    ovFactor = (vec3(1.0/occlusion)/(NUM_SAMPLES));
+    gl_FragColor = vec4(ovFactor, 1);
 } 
 
 

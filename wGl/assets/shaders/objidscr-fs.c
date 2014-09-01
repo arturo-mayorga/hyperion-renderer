a08287a0bfd4ead598923077f26d4be2e25ab662
arturo-mayorga <arturo.mayorga@gmail.com>
2014-09-01 17:14:42 -0400

    Rendering by objId for the HUD.

--- wGl/assets/shaders/objidscr-fs.c
+++ wGl/assets/shaders/objidscr-fs.c
@@ -21,15 +21,12 @@
 precision mediump float;
 
 varying vec2 vTexCoordinate;
-uniform sampler2D uMapKd;
-uniform vec4 uKd;
+
+uniform vec4 uObjid;
+
 
 void main(void)
 {
-	vec3 tColor = texture2D(uMapKd, vTexCoordinate).xyz;
-	vec3 fColor = vec3( min(tColor.x, uKd.x), 
-	                    min(tColor.y, uKd.y), 
-	                    min(tColor.z, uKd.z) );
-	gl_FragColor = vec4(fColor, uKd.a);
+	gl_FragColor = vec4(uObjid.xyz, 1.0);
 } 
 

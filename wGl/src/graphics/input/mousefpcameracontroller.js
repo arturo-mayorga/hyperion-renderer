c998e913e62e2f855f0742d4a2a34ec4d555418b
arturo-mayorga <arturo.mayorga@gmail.com>
2014-09-14 14:50:36 -0400

    Converting the MouseFpCameraController class to a FsmMachine subclass to help us manage the different substates.

--- wGl/src/graphics/input/mousefpcameracontroller.js
+++ wGl/src/graphics/input/mousefpcameracontroller.js
@@ -20,10 +20,14 @@
 
 /**
  * @constructor
+ * @extends {FsmMachine}
  * @implements {IContextMouseObserver}
  */
 function MouseFpCameraController()
 {
+    FsmState.debugEnable = true;
+    FsmMachine.call( this );
+
     this.dEyePos = vec3.create();
     this.dX = this.dY = this.dZ = undefined;
     this.dPitch = this.dRoll = this.dYaw = undefined;
@@ -47,7 +51,141 @@ function MouseFpCameraController()
 	
 	this.horizontalAxis = vec3.create();
 	this.verticalAxis = vec3.fromValues(0,1,0);
+
+    this.mouseDown = false;
+    this.mouseUp = false;
+    this.mouseMove = false;
+
+    this.flyTime = 0;
+
+    this.initStateMachine();
 }
+
+MouseFpCameraController.prototype = Object.create( FsmMachine.prototype );
+
+/**
+ *  Initialize the state transitions
+ */
+MouseFpCameraController.prototype.initStateMachine = function ()
+{
+    this.createSubState( "idle", this.idleEnter, this.idleUpdate, function(){} );
+    this.createSubState( "mouseDnRec", this.mouseDnRecEnter, this.mouseDnRecUpdate, function(){} );
+    this.createSubState( "fly", this.flyEnter, this.flyUpdate, function(){} );
+    this.createSubState( "observe", this.observeEnter, this.observeUpdate, function(){} );
+
+    this.addTransition( "idle", "mouseDown", "mouseDnRec" );
+    this.addTransition( "mouseDnRec", "mouseUp", "fly" );
+    this.addTransition( "mouseDnRec", "mouseMove", "observe" );
+    this.addTransition( "fly", "reachedTarget", "idle" );
+    this.addTransition( "observe", "mouseUp", "idle" );
+
+    this.setName( "mouseFpCameraController" );
+    this.setState( "idle" );
+};
+
+/**
+ * Enter the observe state
+ */
+MouseFpCameraController.prototype.observeEnter = function()
+{
+    this.mouseUp = false;
+};
+
+/**
+ * Update the observe state
+ */
+MouseFpCameraController.prototype.observeUpdate = function()
+{
+    if ( this.mouseUp )
+    {
+        this.fireSignal( "mouseUp" );
+        return;
+    }
+
+    var mvMatrix = mat4.create();
+    mat4.identity( mvMatrix );
+
+    vec3.subtract( this.eyeLookAt, this.eyeLookAtStart, this.eyePosStart );
+
+    // allways rotate around the y axis
+    mat4.rotate( mvMatrix, mvMatrix, this.viewPortDrag[0]*Math.PI, this.verticalAxis );
+
+    // find the horizontal axis
+    mat4.rotate( mvMatrix, mvMatrix, this.viewPortDrag[1]*Math.PI, this.horizontalAxis );
+
+    vec3.transformMat4(this.eyeLookAt, this.eyeLookAt, mvMatrix);
+
+    vec3.add( this.eyeLookAt, this.eyeLookAt, this.eyePosStart );
+
+    this.setValuesToCam();
+};
+
+/**
+ * Enter the fly state
+ */
+MouseFpCameraController.prototype.flyEnter = function()
+{
+    this.flyTime = 0;
+};
+
+/**
+ * Update the fly state
+ * @param {number} time
+ */
+MouseFpCameraController.prototype.flyUpdate = function( time )
+{
+    this.flyTime += time;
+
+    if ( 1000 <= this.flyTime )
+    {
+        this.fireSignal( "reachedTarget" );
+    }
+};
+
+/**
+ * Enter the mouseDnRec state
+ */
+MouseFpCameraController.prototype.mouseDnRecEnter = function()
+{
+    this.mouseUp = false;
+    this.mouseMove = false;
+};
+
+/**
+ * Update the mouseDnRec state
+ * @param {number} time
+ */
+MouseFpCameraController.prototype.mouseDnRecUpdate = function( time )
+{
+    if ( this.mouseUp )
+    {
+        this.fireSignal( "mouseUp" );
+    }
+    else if ( this.mouseMove )
+    {
+        this.fireSignal( "mouseMove" );
+    }
+};
+
+/**
+ * Enter the idle state
+ */
+MouseFpCameraController.prototype.idleEnter = function()
+{
+    this.mouseDown = false;
+};
+
+/**
+ * Update the idle state
+ * @param {number} time
+ */
+MouseFpCameraController.prototype.idleUpdate = function( time )
+{
+    if ( this.mouseDown )
+    {
+        this.fireSignal("mouseDown");
+    }
+};
 
 /**
  * @param {PointingEvent} ev
@@ -77,16 +215,17 @@ MouseFpCameraController.prototype.onMouseDown = function( ev )
 
     vec3.subtract( this.eyeLookAtDir, this.eyeLookAt, this.eyePos );
     vec3.cross( this.eyeRight, this.eyeLookAtDir, this.eyeUp );
-    vec3.copy( this.horizontalAxis, this.eyeRight )
+    vec3.copy( this.horizontalAxis, this.eyeRight );
 
 
     vec3.copy( this.eyePosStart, this.eyePos );
 
 
 
-    vec3.copy( this.eyeLookAtStart, this.eyeLookAt )
+    vec3.copy( this.eyeLookAtStart, this.eyeLookAt );
     vec3.copy( this.eyeUpStart, this.eyeUp );
 
+    this.mouseDown = true;
 
     return true;
 };
@@ -99,6 +238,8 @@ MouseFpCameraController.prototype.onMouseUp = function( ev )
     if ( false === this.isDragging ) return false;
      
     this.isDragging = false;
+
+    this.mouseUp = true;
     
     return true;
 };
@@ -114,6 +255,8 @@ MouseFpCameraController.prototype.onMouseMove = function( ev )
     var viewPortY = ev.getY();
     
     vec2.subtract( this.viewPortDrag, this.viewPortOrigin, new Float32Array([viewPortX, viewPortY]) );
+
+    this.mouseMove = true;
     
     return true;
 };
@@ -143,34 +286,4 @@ MouseFpCameraController.prototype.setValuesToCam = function()
     camera.setEye( this.eyePos[0], this.eyePos[1], this.eyePos[2] );
     camera.setLookAt( this.eyeLookAt[0], this.eyeLookAt[1], this.eyeLookAt[2] );
     camera.setUp( this.eyeUp[0], this.eyeUp[1], this.eyeUp[2] );
-};
-
-/**
- * Update the state of the controller
- * @param {number} time
- */
-MouseFpCameraController.prototype.update = function( time )
-{
-    if ( false === this.isDragging ) return;
-    
-    var mvMatrix = mat4.create();
-    mat4.identity( mvMatrix );
-
-
-    ;
-
-
-    vec3.subtract( this.eyeLookAt, this.eyeLookAtStart, this.eyePosStart );
-    
-    // allways rotate around the y axis
-    mat4.rotate( mvMatrix, mvMatrix, this.viewPortDrag[0]*Math.PI, this.verticalAxis );
-    
-    // find the horizontal axis
-    mat4.rotate( mvMatrix, mvMatrix, this.viewPortDrag[1]*Math.PI, this.horizontalAxis );
-
-    vec3.transformMat4(this.eyeLookAt, this.eyeLookAt, mvMatrix);
-
-    vec3.add( this.eyeLookAt, this.eyeLookAt, this.eyePosStart );
-    
-    this.setValuesToCam();
 };
